henrikedin/mongo

SERVER-32487 Embedded CAPI takes a yaml config string instead of argc and argv.

src/mongo/client/embedded/embedded_options_parser_init.cpp
@@ -1,0 +1,55 @@
+/**
+ *    Copyright (C) 2013 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/util/options_parser/startup_options.h"
+
+#include <iostream>
+
+#include "mongo/util/exit_code.h"
+#include "mongo/util/options_parser/option_description.h"
+#include "mongo/util/options_parser/option_section.h"
+#include "mongo/util/options_parser/options_parser.h"
+#include "mongo/util/options_parser/startup_option_init.h"
+#include "mongo/util/quick_exit.h"
+
+namespace mongo {
+namespace optionenvironment {
+
+MONGO_STARTUP_OPTIONS_PARSE(StartupOptions)(InitializerContext* context) {
+    std::string config_str = !context->args().empty() ? context->args()[0] : "";
+
+    OptionsParser parser;
+    Status ret =
+        parser.runConfigFile(startupOptions, config_str, context->env(), &startupOptionsParsed);
+    uassertStatusOKWithContext(ret, "Options parsing failed.");
+
+    return Status::OK();
+}
+
+}  // namespace optionenvironment
+}  // namespace mongo

src/mongo/util/options_parser/options_parser.h
@@ -110,6 +110,11 @@ public:
                const std::map<std::string, std::string>& env,
                Environment*);
 
+    Status runConfigFile(const OptionSection&,
+                         const std::string& argv,
+                         const std::map<std::string, std::string>& env,
+                         Environment*);
+
 private:
     /** Handles parsing of the command line and adds the results to the given Environment */
     Status parseCommandLine(const OptionSection&,
@@ -119,6 +124,10 @@ private:
     /** Handles parsing of an INI config std::string and adds the results to the given Environment
      * */
     Status parseINIConfigFile(const OptionSection&, const std::string& config, Environment*);
+
+    /** Handles parsing of either YAML or INI config and adds the results to the given Environment
+     */
+    Status parseConfigFile(const OptionSection&, const std::string& argv, Environment*);
 
     /** Gets defaults from the OptionSection and adds them to the given Environment */
     Status addDefaultValues(const OptionSection&, Environment*);

hwCloudDBSDDS/dds

src/mongo/client/embedded/embedded_options_parser_init.cpp
@@ -1,0 +1,57 @@
+/**
+ *    Copyright (C) 2018 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/util/options_parser/startup_options.h"
+
+#include <iostream>
+
+#include "mongo/util/exit_code.h"
+#include "mongo/util/options_parser/option_description.h"
+#include "mongo/util/options_parser/option_section.h"
+#include "mongo/util/options_parser/options_parser.h"
+#include "mongo/util/options_parser/startup_option_init.h"
+#include "mongo/util/quick_exit.h"
+
+namespace mongo {
+namespace optionenvironment {
+
+MONGO_STARTUP_OPTIONS_PARSE(StartupOptions)(InitializerContext* context) {
+    // Embedded uses a YAML config passed in argv to reuse the existing interface, extract it from
+    // the first element otherwise use empty string.
+    std::string config = !context->args().empty() ? context->args()[0] : "";
+
+    OptionsParser parser;
+    Status ret =
+        parser.runConfigFile(startupOptions, config, context->env(), &startupOptionsParsed);
+    uassertStatusOKWithContext(ret, "Options parsing failed.");
+
+    return Status::OK();
+}
+
+}  // namespace optionenvironment
+}  // namespace mongo

src/mongo/util/options_parser/options_parser.h
@@ -110,6 +110,15 @@ public:
                const std::map<std::string, std::string>& env,
                Environment*);
 
+    /** Handles parsing of a YAML or INI formatted string. The
+     *  OptionSection be a description of the allowed options.  This function populates the
+     *  given Environment with the results but does not call validate on the Environment.
+     */
+    Status runConfigFile(const OptionSection&,
+                         const std::string& config,
+                         const std::map<std::string, std::string>& env,
+                         Environment*);
+
 private:
     /** Handles parsing of the command line and adds the results to the given Environment */
     Status parseCommandLine(const OptionSection&,
@@ -119,6 +128,10 @@ private:
     /** Handles parsing of an INI config std::string and adds the results to the given Environment
      * */
     Status parseINIConfigFile(const OptionSection&, const std::string& config, Environment*);
+
+    /** Handles parsing of either YAML or INI config and adds the results to the given Environment
+     */
+    Status parseConfigFile(const OptionSection&, const std::string& argv, Environment*);
 
     /** Gets defaults from the OptionSection and adds them to the given Environment */
     Status addDefaultValues(const OptionSection&, Environment*);